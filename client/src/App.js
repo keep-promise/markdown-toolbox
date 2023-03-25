a3e164ca342c716e5a8ec6e50ea68268bcddda8e
jiangxinqi <jiangxinqi@demodeMacBook-Pro.local>
2023-03-26 00:38:42 +0800

    提交代码

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import Vditor from "vditor";
+// import './App.css';
+// import "vditor/dist/index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// const Veditor = () => {
+//   const [vd, setVd] = React.useState<Vditor>({});
+//   React.useEffect(() => {
+//     const vditor = new Vditor("vditor", {
+//       width: '100%',
+//       minHeight: 400,
+//       mode: 'sv',
+//       after: () => {
+//         vditor.setValue("`Vditor` 最小代码示例");
+//         setVd(vditor);
+//       }
+//     });
+//   }, []);
+//   return <div id="vditor" className="vditor" />;
+// };
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Veditor />
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import "vditor/dist/index.css";
+import React, { useEffect } from "react";
+import Vditor from "vditor";
+
+const App = () => {
+  // const [vd, setVd] = React.useState<Vditor>({});
+  useEffect(() => {
+    const vditor = new Vditor("vditor", {
+      after: () => {
+        vditor.setValue("`Vditor` 最小代码示例");
+        // setVd(vditor);
+      }
+    });
+  }, []);
+  return <div id="vditor" className="vditor" />;
+};
 
 export default App;
