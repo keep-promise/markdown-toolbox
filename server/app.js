ac4ad9703c7d59908c6d7fd8720f5ffbedd2944b
jiangxinqi <jiangxinqi@demodeMacBook-Pro.local>
2023-03-25 10:00:21 +0800

    提交代码

--- server/app.js
+++ server/app.js
@@ -1,9 +1,30 @@
 
-require('dotenv');
+require('dotenv').config({path: '.env'});
 const express = require('express');
-const html = require('./routes/html');
+const { Sequelize } = require('sequelize');
+
+// const html = require('./routes/html');
 
 const app = express();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  logging: false,
+  port: process.env.DB_PORT
+});
+
+const dbConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(`mysql connect success on ${process.env.DB_PORT}`)
+  } catch (error) {
+    console.error('mysql connect fail', error)
+  }
+}
+
+// mysql
+dbConnection()
 
 const PORT = process.env.PORT || 8000;
 
