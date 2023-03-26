a36320d38fd99fb9a34aa722b61fda3db588d105
jiangxinqi <jiangxinqi@demodeMacBook-Pro.local>
2023-03-27 00:59:48 +0800

    提交代码

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,60 @@
-// import React from 'react';
-// import Vditor from "vditor";
-// import './App.css';
-// import "vditor/dist/index.css";
-
-// const Veditor = () => {
-//   const [vd, setVd] = React.useState<Vditor>({});
-//   React.useEffect(() => {
-//     const vditor = new Vditor("vditor", {
-//       width: '100%',
-//       minHeight: 400,
-//       mode: 'sv',
-//       after: () => {
-//         vditor.setValue("`Vditor` 最小代码示例");
-//         setVd(vditor);
-//       }
-//     });
-//   }, []);
-//   return <div id="vditor" className="vditor" />;
-// };
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <Veditor />
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import "vditor/dist/index.css";
-import React, { useEffect } from "react";
+import React from 'react';
 import Vditor from "vditor";
+import './App.css';
+import "vditor/dist/index.css";
+import { get } from './api';
 
-const App = () => {
+const Veditor = () => {
   // const [vd, setVd] = React.useState<Vditor>({});
-  useEffect(() => {
+  React.useEffect(() => {
     const vditor = new Vditor("vditor", {
+      width: '100%',
+      minHeight: 400,
+      mode: 'sv',
       after: () => {
         vditor.setValue("`Vditor` 最小代码示例");
-        // setVd(vditor);
       }
     });
   }, []);
-  return <div id="vditor" className="vditor" />;
+  return <div id="vditor" />;
 };
 
+function App() {
+
+  const gethtml = () => {
+    get('/gethtml').then(res => {
+      console.log('gethtml', res)
+    })
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="text-int">Write markdown nice & easy</div>
+      </header>
+      <div className="App-content">
+        <div className="veditor-container">
+          <div className="url-submit">
+            <input 
+              type="text" 
+              class="form-control" 
+              placeholder="请输入url"
+            >
+            </input>
+            <button 
+              type="button" 
+              class="btn-secondary"
+              onClick={gethtml}
+            >
+              一键转换
+            </button>
+          </div>
+          <Veditor />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default App;
+
